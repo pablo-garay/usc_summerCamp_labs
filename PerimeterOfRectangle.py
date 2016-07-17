d4793989f4873de71d1d4a415194dbda9e1e006f
Pablo Garay <pgaray@roshka.com>
2016-07-16 20:37:32 -0700

    Added Perimeter of Rectangle exercise solution

---
+++ PerimeterOfRectangle.py
@@ -0,0 +1,3 @@
+length = float(input("Enter length of rectangle: "))
+width  = float(input("Enter width of rectangle: "))
+print("The rectangle with length " + str(length) + " and width " + str(width) + " has a perimeter of " + str(2 * (length + width)) + ".")
