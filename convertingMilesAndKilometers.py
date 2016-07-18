5dffc36b4de2030d6f237e9099b51e05abeeb06b
Pablo Garay <pgaray@roshka.com>
2016-07-18 10:13:44 -0700

    Added Converting Between Miles and Kilometers exercise solution

---
+++ convertingMilesAndKilometers.py
@@ -0,0 +1,8 @@
+numMiles = 10.3
+
+print("Number of Miles Given – %f" %numMiles)
+print("Number of Kilometers in %f Miles – %f" %(numMiles, numMiles * 1.60934))
+
+numKilometers = 23.7
+print("Number of Kilometers Give – %f" %numKilometers)
+print("Number of Miles in %f Kilometers – %f" %(numKilometers, numKilometers / 1.60934))
