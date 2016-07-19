389aa89c2d17b9ebae4f4cc0c908a08e745c9c21
Pablo Garay <pgaray@roshka.com>
2016-07-19 13:44:52 -0700

    Added Solving Exponent Problems exercise solution

--- solvingExponentProblems.py
+++ solvingExponentProblems.py
@@ -1,5 +1,11 @@
+from sys import argv
+
+if (len(argv) != 3):
+    print("Usage: Include two command line parameters x and y to find x^y")
+    exit(1)
+
 print("Program that raises x to y i.e. it finds x^y")
-x = float(input("x: "))
-y = float(input("y: "))
+x = float(argv[1])
+y = float(argv[2])
 
 print("%f^%f = %f" %(x, y, x ** y))
