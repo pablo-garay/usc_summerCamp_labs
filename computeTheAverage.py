34d98bc54020dd319829b4d0e6ef5402a280f794
Pablo Garay <pgaray@roshka.com>
2016-07-19 16:54:34 -0700

    Added Temperature Converter, Football Score Calculator solutions

---
+++ computeTheAverage.py
@@ -0,0 +1,10 @@
+total = 0
+
+total += int(input("Enter #1"))
+total += int(input("Enter #2"))
+total += int(input("Enter #3"))
+total += int(input("Enter #4"))
+total += int(input("Enter #5"))
+
+print("The sum is %d" %total)
+print("The average is %d" %total / 5)
