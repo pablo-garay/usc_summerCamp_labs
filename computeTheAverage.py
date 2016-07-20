b1c85ad21f14300c16685c3c105c17b1c177eac8
Pablo Garay <pgaray@roshka.com>
2016-07-19 17:00:10 -0700

    Fixed Compute the Average solution

--- computeTheAverage.py
+++ computeTheAverage.py
@@ -1,10 +1,10 @@
 total = 0
 
-total += int(input("Enter #1"))
-total += int(input("Enter #2"))
-total += int(input("Enter #3"))
-total += int(input("Enter #4"))
-total += int(input("Enter #5"))
+total += float(input("Enter #1 - "))
+total += float(input("Enter #2 - "))
+total += float(input("Enter #3 - "))
+total += float(input("Enter #4 - "))
+total += float(input("Enter #5 - "))
 
-print("The sum is %d" %total)
-print("The average is %d" %total / 5)
+print("The sum is %f" %total)
+print("The average is %f" %(total / 5))
