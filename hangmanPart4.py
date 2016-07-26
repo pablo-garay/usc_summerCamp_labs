fefd3d4d8094f26d883b4693a61d3e005ce33c3f
Pablo Garay <pgaray@roshka.com>
2016-07-25 22:27:35 -0700

    hangman part 4

---
+++ hangmanPart4.py
@@ -0,0 +1,25 @@
+print("Welcome to Hangman!")
+
+secretWord = str(input("Enter the secret word: "))
+print("Length of secret word: %d" %len(secretWord))
+
+def isCharInString(word, ch):
+    if ch in word:
+        return True
+    else:
+        return False
+
+incorrect_guesses = 0
+
+while(incorrect_guesses < 7):
+    user_str = str(input("\nEnter a letter: "))
+
+    # check if user_str is in string
+    if isCharInString(secretWord, user_str):
+        print("%s IS in the secret word at index %d." % (user_str, secretWord.find(user_str)))
+    else:
+        print("%s is NOT in the secret word." % user_str)
+        incorrect_guesses += 1
+        print("That is incorrect guess #%d.\n" %incorrect_guesses)
+
+print("Game over.")
