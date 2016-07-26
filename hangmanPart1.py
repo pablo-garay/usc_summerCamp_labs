127f1940a36fd650e1f1c37c40bac17eb0a89c13
Pablo Garay <pgaray@roshka.com>
2016-07-25 22:01:20 -0700

    Added solution to Hangman Part 1

--- hangmanPart1.py
+++ hangmanPart1.py
@@ -2,9 +2,16 @@ print("Welcome to Hangman!")
 
 secretWord ="hello"
 
-def isCharInString(word, ch):
+def isuser_strInString(word, ch):
     if ch in word:
         return True
     else:
         return False
 
+user_str = str(input("Enter a letter: "))
+
+# check if user_str is in string
+if isuser_strInString(secretWord, user_str):
+    print("%s IS in the secret word." % user_str)
+else:
+    print("%s is NOT in the secret word." % user_str)
